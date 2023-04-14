2deea9d308c0c529bfafce08c05581a533d9e651
Robert Schmidt <r.schmidt@dotsource.de>
2023-04-14 12:53:04 +0200

    fixed linting

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -7,6 +7,21 @@ interface PlayersProps {
     playerData: { avatar: string; name: string; id: string }[];
 }
 
+interface Player {
+  attributes: {
+    image: {
+      data: {
+        attributes: {
+          url: string;
+        };
+      };
+    };
+    name: string;
+  };
+  id: string;
+}
+
+
 const PlayersPage: NextPage<PlayersProps> = ({ playerData }) => {
     return (
         <>
@@ -21,7 +36,7 @@ const PlayersPage: NextPage<PlayersProps> = ({ playerData }) => {
 export const getServerSideProps: GetServerSideProps<PlayersProps> = async (context) => {
     const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_HOST + '/api/players?populate=*');
     const resJSON = await res.json();
-    const players = resJSON.data.map((item) => {
+    const players = resJSON.data.map((item: Player) => {
         return {
             avatar: item.attributes.image.data ? process.env.NEXT_PUBLIC_STRAPI_HOST + item.attributes.image.data.attributes.url : "",
             name: item.attributes.name,
